0f2035d6a37e1b5dee940f7954c476955fc58d27
James Blachly <james.blachly@gmail.com>
2019-04-03 04:30:29 +0000

    vcfcopy, another minimal example segfaults

--- vcfcopy.c
+++ vcfcopy.c
@@ -12,22 +12,8 @@ int main(void)
     bcf_hdr_t* infile_hdr = bcf_hdr_read(infile);
     bcf_hdr_t* outfile_hdr = bcf_hdr_dup(infile_hdr);
 
-
-    //bcf_hdr_append(outfile_hdr, "##INFO=<ID=refchg,Number=0,Type=Flag,Description=\"REF allele changed in new genome\">");
-    
-    int nseqs;
-    const char** ary = bcf_hdr_seqnames(outfile_hdr, &nseqs);
-
-    /*
-    for(int i=0; i < nseqs ; i++)
-    {
-	    // TODO also try passing NULL as final param without loop
-	    bcf_hdr_remove(outfile_hdr, BCF_HL_CTG, ary[i]);
-    }
-    */
+    // uncomment below to increase frequency of segfault; likewise you can add a second header line to increase probabilty further
     bcf_hdr_append(outfile_hdr, "##contig=<ID=chrZ,length=57227415,source=resources/hg19ToHg38.over.chain>");
-    
-    bcf_hdr_append(outfile_hdr, "##filedate=20190403");
     
     bcf_hdr_write(outfile, outfile_hdr);
 
@@ -38,11 +24,16 @@ int main(void)
     while( bcf_read(infile, infile_hdr, b) >= 0 )
     {
 	bcf1_t *bnew = bcf_dup(b);
-	int ret = bcf_unpack(bnew, MAX_UNPACK);
-
-	bcf_translate(outfile_hdr, infile_hdr, bnew);
+ 	int ret = bcf_unpack(bnew, MAX_UNPACK);
 	
-	ret = bcf_write(outfile, outfile_hdr, bnew);
+	bcf_translate(outfile_hdr, infile_hdr, bnew);
+
+	if (bnew->d.n_flt > 1) break;
+
+	bcf_write(outfile, outfile_hdr, bnew);
+
+	bcf_destroy(bnew);
+	bcf_empty(b);
     }
     
     bcf_close(infile);
